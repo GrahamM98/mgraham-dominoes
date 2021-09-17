e19179e27a90aeaa072435e120362f3f2b5d45e8
Michael Graham <michael@thegrahamfam.com>
2021-09-16 21:09:40 -0500

    react app init
    Added base react app, as well as changes to allow it to communicate with the api

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, send_from_directory
+from flask_restful import Api, Resource, reqparse
+from flask_cors import CORS #comment this on deployment
+from api.TestConnect import TestConnect
+
+app = Flask(__name__, static_url_path='', static_folder='frontend/build')
+CORS(app) #comment this on deployment
+
+api = Api(app)
+
+@app.route("/", defaults={'path':''})
+def serve(path):
+    return send_from_directory(app.static_folder,'index.html')
+
+api.add_resource(TestConnect, '/test_connect')
